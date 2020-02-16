b4d408cdd84a5a6e674e0a098b67bb8da76acef1
arellyragland <arellyragland@gmail.com>
2020-02-16 14:08:02 -0500

    Added Code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Second line of code!");
+            Console.WriteLine("Third line of code!");
+            Console.WriteLine("Fourth line of code!");
+        }
+    }
+}
