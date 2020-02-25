86bb3cee837efed154a8aa276a00a265072b880b
arellyragland <arellyragland@gmail.com>
2020-02-24 22:40:23 -0500

    For,While and Do-While loop commit

---
+++ loops/loops/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            const int MAX = 5;
+
+            for (int i = 0; i < MAX; i++)
+
+            {
+
+                System.Console.WriteLine("For Loop : {0}", i);
+
+            }
+
+
+
+            int j = 0;
+
+            while (j < MAX)
+
+            {
+
+                System.Console.WriteLine("While Loop : {0}", j);
+
+                j++;
+
+            }
+
+
+
+            int k = 0;
+
+            do
+
+            {
+
+                System.Console.WriteLine("Do While Loop : {0}", k);
+
+                k++;
+
+            } while (k < MAX);
+        }
+    }
+}
