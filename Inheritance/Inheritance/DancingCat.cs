1485ba5917225ed7e6442f674981ca99e0b4619f
arellyragland <arellyragland@gmail.com>
2020-03-08 12:27:21 -0400

    Inheritance Commit

---
+++ Inheritance/Inheritance/DancingCat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class DancingCat : Cat
+    {
+        string danceMove;
+
+        public DancingCat(string danceMove, string catName) : base(catName, false)
+        {
+            this.danceMove = danceMove;
+        }
+
+        public void Dance()
+        {
+            Console.WriteLine(this.name + " dances. She does the " + danceMove);
+        }
+
+    }
+}
