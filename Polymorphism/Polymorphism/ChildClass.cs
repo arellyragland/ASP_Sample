8c06963b5d7850d14464164472cf2199aade2d31
arellyragland <arellyragland@gmail.com>
2020-03-17 09:23:43 -0500

    Polymorphism Commit

---
+++ Polymorphism/Polymorphism/ChildClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class ChildClass : SimpleClass
+    {
+        public ChildClass() : base()
+        {
+
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/SimpleClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class SimpleClass
+    {
+        public SimpleClass() { }
+
+
+        //Static Polymorphism , Method Overloading
+        public int Add(int a, int b)
+        {
+            int results = a + b;
+            Console.WriteLine(results);
+            return results;
+        }
+        public int Add(int a, int b, int c)
+        {
+            int results = a + b + c;
+            Console.WriteLine(results);
+            return results;
+        }
+        public string Add(string a, string b)
+        {
+            string results = a + " " + b;
+            Console.WriteLine(results);
+            return results;
+        }
+    }
+}
