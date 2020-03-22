6208adf2517d489c89438945de2096cd365122a9
arellyragland <arellyragland@gmail.com>
2020-03-22 00:37:32 -0500

    Interface Commit

---
+++ Interface/Interface/Oven.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    class Oven
+    {
+        public Oven()
+        {
+
+        }
+
+        public void Bake(IPizza pizza)
+        {
+            Console.WriteLine("I baked a " + pizza.GetType() + " pizza.");
+        }
+    }
+}
