e6b629622b6dea52fe4121e71f68c83e094c961b
arellyragland <arellyragland@gmail.com>
2020-03-23 23:35:45 -0500

    Database Connection Commit

---
+++ DatabaseConnection/DatabaseConnection/SakilaContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DatabaseConnection
+{
+    class SakilaContext : DbContext
+    {
+        public DbSet<Actor> Actor { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-GCR864HL\SQLEXPRESS;Database=sakila;Trusted_Connection=True;");
+        }
+    }
+}
