69171051bd48d187bd19593f835bbc0b1b5dd8af
arellyragland <arellyragland@gmail.com>
2020-03-26 11:28:09 -0500

    Daabase_Lab Commit

---
+++ Database_Lab/Database_Lab/SakilaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_Lab
+{
+        class SakilaContext : DbContext
+        {
+
+            public DbSet<Film> Film { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlServer(@"Server=LAPTOP-GCR864HL\SQLEXPRESS;Database=sakila;Trusted_Connection=True;")
+    ;
+            }
+        }
+    
+}
