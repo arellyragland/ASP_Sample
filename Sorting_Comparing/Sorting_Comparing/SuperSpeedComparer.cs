04d3eff34e3be8700be66e9bc3585cf9cc72513e
arellyragland <arellyragland@gmail.com>
2020-03-28 17:04:35 -0500

    Sort & Compare Commit

---
+++ Sorting_Comparing/Sorting_Comparing/SuperSpeedComparer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting_Comparing
+{
+    class SuperSpeedComparer : Comparer<SuperHero>
+    {
+
+        public override int Compare(SuperHero x, SuperHero y)
+        {
+            if (x.speed > y.speed)
+                return -1;
+            else if (x.speed < y.speed)
+                return 1;
+            else
+                return 0;
+
+
+        }
+    }
+
+    class SuperStrengthComparer : Comparer<SuperHero>
+    {
+
+        public override int Compare(SuperHero x, SuperHero y)
+        {
+            if (x.strength > y.strength)
+                return -1;
+            else if (x.strength < y.strength)
+                return 1;
+            else
+                return 0;
+
+
+        }
+    }
+
+
+
+
+}
